e1f1e8410c4d5927b6886dfd76e670e89c4cce84
张先林 <zhangxl@wangji.asia>
2019-05-23 00:51:21 +0800

    add main.py to debug

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ ='xianlin'
+
+from scrapy.cmdline import execute
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(['scrapy','crawl','jobbole'])
